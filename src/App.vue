9621787dc450e2d66c04d6588e13c5aa8d8e01dc
bazileros <zalisilesithule@gmail.com>
2024-10-21 10:56:13 +0200

    [FIX] underterminated strings

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import NavBar from '@/components/Navbar'
-import FooterBar from '@/components/FooterBar
+import FooterBar from '@/components/FooterBar'
 </script>
 
 <template>
