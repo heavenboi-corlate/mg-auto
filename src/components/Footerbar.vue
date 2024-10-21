18c8ce4f75204b3ed94a0801b1c99e5de88dcc78
bazileros <zalisilesithule@gmail.com>
2024-10-21 10:47:32 +0200

    [ADD] footer component

---
+++ src/components/Footerbar.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue';
+
+const currentYear = new Date().getFullYear();
+</script>
+
+<template>
+  <footer class="bg-slate-800 p-4">
+    <p class="text-center text-white">
+      {{ currentYear }} My Company. All rights reserved.
+    </p>
+  </footer>
+</template>
