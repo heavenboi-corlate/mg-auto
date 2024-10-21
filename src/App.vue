cafa82e429f42d11ec06b04164e3577547d55e3a
bazileros <zalisilesithule@gmail.com>
2024-10-21 11:44:46 +0200

    [FIX] Import statements

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-import { RouterView } from 'vue-router';
-import NavBar from './components/Navbar';
-import Footer from './components/Footer';
+import { RouterView } from "vue-router";
+import NavBar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <NavBar />
   <RouterView />
-  <Footer/>
+  <Footer />
 </template>
