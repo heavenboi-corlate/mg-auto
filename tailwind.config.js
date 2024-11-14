4886e08254705ab0dd004b51443e800497da5235
bazileros <zalisilesithule@gmail.com>
2024-11-13 19:49:06 -0800

    update config for framermotion and animate

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,29 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		screen: {
+		screens: {
 			sm: "640px",
 			md: "768px",
 			lg: "1024px",
 			xl: "1280px",
 			"2xl": "1536px",
 		},
-
-		extend: {},
+		extend: {
+			keyframes: {
+				fadeIn: {
+					"0%": { opacity: "0", transform: "translateY(10px)" },
+					"100%": { opacity: "1", transform: "translateY(0)" },
+				},
+				fadeOut: {
+					"0%": { opacity: "1" },
+					"100%": { opacity: "0" },
+				},
+			},
+			animation: {
+				"fade-in": "fadeIn 0.5s ease-in forwards",
+				"fade-out": "fadeOut 0.5s ease-out forwards",
+			},
+		},
 	},
 	plugins: [],
 };
