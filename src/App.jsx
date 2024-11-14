6f0378fbf6a1510224e67d5fa2698b0cc2bd3544
bazileros <zalisilesithule@gmail.com>
2024-11-13 20:41:21 -0800

    fixed import of nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import LoadingSpinner from "./components/LoadingSpinner";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBar";
 import Hero from "./components/Hero";
 import AboutUs from "./components/AboutUs";
 import Service from "./components/Service";
